94a8b077d44a027153bc230899e6177c87841b73
turistik75rus <turistik75rus@mail.ru>
2021-06-25 15:04:09 +0300

    e

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main');
-});
-
-Route::get('/login', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@index');
